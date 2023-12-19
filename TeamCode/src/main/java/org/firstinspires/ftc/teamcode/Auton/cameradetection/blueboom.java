4734252da20c82ad9ca8cff50edfc6eb5a032e69
carloscedano <carlos@nycfirst.org>
2023-12-19 00:01:45 -0500

    Auton Detection - RED - TESTING REQUIRED

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton/cameradetection/blueboom.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton/cameradetection/blueboom.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.Auton;
+package org.firstinspires.ftc.teamcode.Auton.cameradetection;
 
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.Vector2d;
@@ -15,10 +15,10 @@ import org.opencv.core.Scalar;
 
 import java.util.concurrent.TimeUnit;
 
-@Autonomous(name = "Left Side Blue", group = "Blue Autos")
-public class boom extends OpMode {
+@Autonomous
+public class blueboom extends OpMode {
     private VisionPortal visionPortal;
-    private boomutil blueTram;
+    private boomutil blueProp;
 
     @Override
     public void init() {
@@ -27,7 +27,7 @@ public class boom extends OpMode {
         Scalar upper = new Scalar(140,255,255);
         double minArea = 100;
 
-        blueTram = new boomutil(
+        blueProp = new boomutil(
                 lower,
                 upper,
                 () -> minArea,
@@ -36,7 +36,7 @@ public class boom extends OpMode {
         );
         visionPortal = new VisionPortal.Builder()
                 .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
-                .addProcessor(blueTram)
+                .addProcessor(blueProp)
                 .build();
 
         try {
@@ -77,10 +77,10 @@ public class boom extends OpMode {
 
     @Override
     public void init_loop() {
-        telemetry.addData("Currently Recorded Position", blueTram.getRecordedPropPosition());
+        telemetry.addData("Currently Recorded Position", blueProp.getRecordedPropPosition());
         telemetry.addData("Camera State", visionPortal.getCameraState());
-        telemetry.addData("Currently Detected Mass Center", "x: " + blueTram.getLargestContourX() + ", y: " + blueTram.getLargestContourY());
-        telemetry.addData("Currently Detected Mass Area", blueTram.getLargestContourArea());
+        telemetry.addData("Currently Detected Mass Center", "x: " + blueProp.getLargestContourX() + ", y: " + blueProp.getLargestContourY());
+        telemetry.addData("Currently Detected Mass Area", blueProp.getLargestContourArea());
     }
 
     @Override
@@ -90,7 +90,7 @@ public class boom extends OpMode {
             visionPortal.stopStreaming();
         }
 
-        boomutil.PropPositions recordedPropPosition = blueTram.getRecordedPropPosition();
+        boomutil.PropPositions recordedPropPosition = blueProp.getRecordedPropPosition();
 
         if (recordedPropPosition == boomutil.PropPositions.UNFOUND) {
             recordedPropPosition = boomutil.PropPositions.MIDDLE;
@@ -118,7 +118,7 @@ public class boom extends OpMode {
 
     @Override
     public void stop() {
-        blueTram.close();
+        blueProp.close();
         visionPortal.close();
     }
 }
