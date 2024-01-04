2d7b150f981efbe4e01ca7da5364117c54219d30
Cherrylaserbeam <146216317+Cherrylaserbeam@users.noreply.github.com>
2024-01-04 08:50:58 -0500

    Ugo's edits to Autonomous

--- FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
+++ FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
@@ -8,5 +8,5 @@ public final class BuildConfig {
   public static final String LIBRARY_PACKAGE_NAME = "com.qualcomm.ftcrobotcontroller";
   public static final String BUILD_TYPE = "debug";
   // Field from default config.
-  public static final String APP_BUILD_TIME = "2023-12-09T00:09:51.812-0500";
+  public static final String APP_BUILD_TIME = "2023-12-18T11:40:17.669-0500";
 }
