52208f18e99fdee2de2756fd0a23a8be91d53ce1
Carlos <139269410+carloscedano@users.noreply.github.com>
2024-01-10 12:06:29 -0500

    boom

--- FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
+++ FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
@@ -8,5 +8,9 @@ public final class BuildConfig {
   public static final String LIBRARY_PACKAGE_NAME = "com.qualcomm.ftcrobotcontroller";
   public static final String BUILD_TYPE = "debug";
   // Field from default config.
+<<<<<<< HEAD
   public static final String APP_BUILD_TIME = "2024-01-08T12:03:02.114-0500";
+=======
+  public static final String APP_BUILD_TIME = "2024-01-10T11:40:40.781-0500";
+>>>>>>> 447324720dd35409b3b9598a4a9f750e17312d23
 }
